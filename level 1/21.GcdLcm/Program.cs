6bae33ae9bcd93f369a555805c7a8bc5203f48a7
shraddhamagar23 <shraddhamagar23@gmail.com>
2026-04-10 15:37:03 +0530

    Add files via upload

---
+++ level 1/21.GcdLcm/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+
+class GcdLcm
+{
+    static void Main()
+    {
+        int num1, num2, gcd, lcm;
+
+        Console.Write("Enter first number: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        int a = num1;
+        int b = num2;
+
+        // Euclidean Algorithm for GCD
+        while (b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        gcd = a;
+
+        // Formula for LCM
+        lcm = (num1 * num2) / gcd;
+
+        Console.WriteLine("GCD = " + gcd);
+        Console.WriteLine("LCM = " + lcm);
+    }
+}
